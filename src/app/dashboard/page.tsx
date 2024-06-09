b92846250e9faeb2ae01244c2399d5b1820a8299
Santiago <victor.lacsilva0102@gmail.com>
2024-06-09 16:00:01 -0300

    file colocation on Next

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+function PageChart() {
+  return (
+    <div>
+      <h1>Page Chart</h1>
+    </div>
+  );
+}
+
+export default function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+    </div>
+  );
+}
